cf376b08801d8105b6e0b41546382859045f2191
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-11-29 19:04:52 +0530

    Add hist eq method

--- src/colorCorrect/histogramEqualizeMethod.cpp
+++ src/colorCorrect/histogramEqualizeMethod.cpp
@@ -1,5 +1,6 @@
 #include <opencv2/core/mat.hpp>
 #include "colorCorrect/histogramEqualizeMethod.hpp"
+#include "process/process.hpp"
 
 using namespace cv;
 
@@ -8,5 +9,6 @@ HistogramEqualizeMethod::HistogramEqualizeMethod() {}
 HistogramEqualizeMethod::~HistogramEqualizeMethod() {}
 
 void HistogramEqualizeMethod::colorCorrect(Mat &img1, Mat &img2) {
-
+    histogramEqualize(img1);
+    histogramEqualize(img2);
 }
