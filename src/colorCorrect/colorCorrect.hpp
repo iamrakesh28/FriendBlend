ce2f624356557c51e586179596c0447dcb61ae92
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-11-30 19:05:50 +0530

    Add doc

--- src/colorCorrect/colorCorrect.hpp
+++ src/colorCorrect/colorCorrect.hpp
@@ -1,6 +1,11 @@
 #ifndef COLOR_CORRECT_HPP
 #define COLOR_CORRECT_HPP
 
+/**
+ * This is the header file you want to import if you
+ * want all the functionality of Color Correction
+ */
+
 #include "colorCorrect/colorCorrection.hpp"
 #include "colorCorrect/histogramEqualizeMethod.hpp"
 #include "colorCorrect/claheMethod.hpp"
