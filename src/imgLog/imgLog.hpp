55b8d8eb0ba45d6653a5e47acb5b0ba5169e9fa8
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-12-01 19:27:01 +0530

    Add doc

--- src/imgLog/imgLog.hpp
+++ src/imgLog/imgLog.hpp
@@ -8,34 +8,51 @@
 using namespace std;
 using namespace cv;
 
+/** Singleton Logger Class */
 class ImageLog {
     private:
 
+    /** Directory where logged images would appear */
     const string logDir;
-    int counter;
-    bool imgLog, addCounterToImageName;
 
+    /** Counter to mark current logged image */
+    int counter;
+
+    /** Should log image or not */
+    bool imgLog;
+    
+    /** Should Add the image count to image name or not */
+    bool addCounterToImageName;
+
+    /** The logger */
     static ImageLog *logger;
 
+    /** Private Constructor */
     ImageLog();
 
     public:
 
+    /** Destructor */
     ~ImageLog();
 
+    /** Should enable logging */
     inline void enableLogging(bool imgLog_) {imgLog = imgLog_;}
 
+    /** Is logging enabled */
     inline bool enabled() {return imgLog;}
 
+    /** Should add counter to image name */
     inline void shouldAddCounterToImageName(bool addCounterToImageName_) {
         addCounterToImageName = addCounterToImageName_;
     }
 
+    /** Log provided image with provided name if logging is enabled */
     void logImage(
         const Mat &image, 
         const string imageName = "image"
     );
 
+    /** Get the logger ! */
     static ImageLog& getLogger();
 };
 
