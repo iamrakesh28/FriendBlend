cab844f19f20caaa9a95a882cec446e10ec0adad
Ahmed Zaheer Dadarkar <ahmed.dadarkar@gmail.com>
2020-12-02 02:12:09 +0530

    Update

--- src/test/friendBlend.cpp
+++ src/test/friendBlend.cpp
@@ -4,6 +4,7 @@
 #include <opencv2/imgcodecs.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <imgLog/imgLog.hpp>
 #include <friendBlend/friendBlend.hpp>
 
 using namespace std;
@@ -11,10 +12,13 @@ using namespace cv;
 
 int main(int argc, char *argv[]) {
 
-    assert (argc == 3);
+    assert (argc == 3 || argc == 4);
 
     Mat friend1 = imread(string(argv[1]), IMREAD_COLOR);
     Mat friend2 = imread(string(argv[2]), IMREAD_COLOR);
+
+    if(argc == 4 && (string(argv[3]) == "log"))   
+        ImageLog::getLogger().enableLogging(true);
 
     assert (!friend1.empty());
     assert (!friend2.empty());
